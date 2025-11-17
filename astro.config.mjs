3413c5bfef357b467c7e799116d6d90068b88f65
realahnet <syedmusicrealise@gmail.com>
2025-11-17 17:11:45 +0500

    Posts: Implement rehype-slug
    * Add id's to headings so that we can:
    * Use rehype-autolink-headings to add the ability to link to a particular section.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["anchor-link"],
+          },
+        },
+      ],
+    ],
   },
 });
